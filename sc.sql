fc89a082f34ea414900f01c9d1d3d525cee5de8e
whoBcode <tru.bone@icloud.com>
2025-09-04 19:48:30 -0500

    fixed sql

--- sc.sql
+++ sc.sql
@@ -1,9 +1,7 @@
--- Clear the table if it exists to start fresh
 DROP TABLE IF EXISTS logs;
-
--- Create the logs table
 CREATE TABLE logs (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
   template TEXT NOT NULL,
   data TEXT NOT NULL
+);
